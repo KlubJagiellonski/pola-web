aab7ebd05a428f98c31b15d6dcae2008f01c2d19
Kamil Breguła <kamil.bregula@polidea.com>
2020-11-01 03:53:31 +0100

    Add tests for deviceId.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,12 +4,13 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 
-// src/setupTests.js
 import { server } from './mocks/server.js'
-// Establish API mocking before all tests.
+
 beforeAll(() => server.listen())
-// Reset any request handlers that we may add during the tests,
-// so they don't affect other tests.
+
 afterEach(() => server.resetHandlers())
-// Clean up after the tests are finished.
+
 afterAll(() => server.close())
+
+// fake device-id
+beforeEach(() => localStorage.setItem('DEVICE_ID', 'WEB-FAKE-ID'))
