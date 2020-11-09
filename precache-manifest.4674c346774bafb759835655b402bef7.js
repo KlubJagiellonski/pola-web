568856d7d22a74e6eff8eb890760bea943059301
mik-laj <mik-laj@users.noreply.github.com>
2020-11-09 22:33:44 +0000

    Update gh-pages to output generated at 2cc1b6b

--- precache-manifest.4674c346774bafb759835655b402bef7.js
+++ precache-manifest.4674c346774bafb759835655b402bef7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3500a52e1cf3fe11ac0e95e73c4643f1",
+    "revision": "fb95a47d5fd9f936a576e58aaf029ab9",
     "url": "https://klubjagiellonski.github.io/pola-web/index.html"
   },
   {
-    "revision": "66958309458ebb06e0ed",
+    "revision": "3c71b2bc256cae94b068",
     "url": "https://klubjagiellonski.github.io/pola-web/static/css/main.1a4511c6.chunk.css"
   },
   {
-    "revision": "fd0f498718c510c067eb",
-    "url": "https://klubjagiellonski.github.io/pola-web/static/js/2.b8c6712d.chunk.js"
+    "revision": "8f38d537f53c825b5999",
+    "url": "https://klubjagiellonski.github.io/pola-web/static/js/2.4f7a5db3.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "https://klubjagiellonski.github.io/pola-web/static/js/2.b8c6712d.chunk.js.LICENSE.txt"
+    "url": "https://klubjagiellonski.github.io/pola-web/static/js/2.4f7a5db3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "66958309458ebb06e0ed",
-    "url": "https://klubjagiellonski.github.io/pola-web/static/js/main.f6bd2f35.chunk.js"
+    "revision": "3c71b2bc256cae94b068",
+    "url": "https://klubjagiellonski.github.io/pola-web/static/js/main.ee5cffd3.chunk.js"
   },
   {
     "revision": "a080cc0e4fa3bc562fc0",
