e019a2804cc5809d022a49287c38d564f3b1eaad
Kamil Breguła <mik-laj@users.noreply.github.com>
2020-11-29 09:15:41 +0100

    Update src/App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-xtest('renders search button', () => {
+test('renders search button', () => {
   const { getByText } = render(<App />);
   const button = getByText(/sprawdź/i);
   expect(button).toBeInTheDocument();
