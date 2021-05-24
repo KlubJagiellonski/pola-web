9438bbb46d5fc2f20d3e35632580e2a37dfea419
zuzanna dudzinska <zuzanna.dudzinska@mayeryn.com>
2021-05-24 18:05:43 +0200

    fix components height and files contents

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
-import { Wrapper, MockUp, Info, AboutTitle, AboutText, AboutButton } from './About.css';
-import { color, fontSize } from '../styles/theme';
+import styled from 'styled-components';
+
+import {padding, margin, color, fontSize} from '../styles/theme';
 import { ButtonColor } from './buttons/Button';
+import { WrapperSection, Text, TitleSection } from '../styles/GlobalStyle.css';
+import { SecondaryButton } from './buttons/SecondaryButton';
+
+const Wrapper = styled(WrapperSection)`
+  min-height: 32.3em;
+  grid-area: about;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column-reverse;
+`;
+
+const MockUp = styled.div`
+  background-color: ${color.background.primary};
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  p{
+    margin: 0;
+    color: ${color.text.secondary};
+    font-size: ${fontSize.small};
+  }
+`
+
+const Info = styled.div`
+  background-color: ${color.background.dark};
+  padding: ${padding.normal} ${padding.big};
+`
+
+const AboutText = styled(Text)`
+  color: ${color.text.light};
+  margin-top: ${margin.normal};
+`
+
+const AboutTitle = styled(TitleSection)`
+  color: ${color.text.light};
+`
+const AboutButton = styled(SecondaryButton)`
+  margin: ${margin.normal} 0;
+`
 
 const About = () => {
   return (
