21d9a2f730e767de15d79bab69662e5d68456c57
Zuzanna Dudzinska <zuzannadudzinska96@gmail.com>
2021-09-15 09:59:07 +0200

    fixed styles

--- src/components/accordion/AccordionList.tsx
+++ src/components/accordion/AccordionList.tsx
@@ -1,40 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Accordion } from 'react-accessible-accordion';
-import { urls } from '../../domain/website';
 
-import './accordion.css';
 import SingleAccordion from './SingleAccordion';
 
-const faq = [
-  {
-    question: "Mam telefon z systemem operacyjnym Android. Nie mogę znaleźć aplikacji w Google Play. Dlaczego?",
-    answer: <p>Aplikacja Pola działa na telefonach wyposażonych w system Android 4.1 lub nowszy. Aktualnie nie planujemy wsparcia dla wcześniejszych wersji.</p>
-  },
-  {
-    question: "Mam telefon iPhone (iPad). Nie mogę znaleźć aplikacji w App Store. Dlaczego?",
-    answer: <p>Aplikacja Pola działa na urządzeniach wyposażonych w system operacyjny iOS w wersji 11.0 lub nowszej. Aktualnie nie planujemy wsparcia dla wcześniejszych wersji.</p>
-  },
-  {
-    question: "Reprezentuję producenta. Moja firma nie została jeszcze zweryfikowana przez redakcję. Co mam zrobić?",
-    answer: <p>W pierwszej kolejności zajmujemy się tymi firmami, których produkty są najczęściej skanowane. Jednak możesz nam pomóc w weryfikacji firmy wypełniając <a href={urls.external.form}>formularz</a>.</p>
-  },
-  {
-    question: "Jak mogę zgłosić uwagi lub błąd w serwisie Pola?",
-    answer: <p>Korzystamy z serwisu GitHub do zgłaszania błędów i uwag. Wybierz do jakiej części Poli chcesz zgłosić uwagi: <a href={urls.external.githubAndroid}>aplikacja na Androida</a>, <a href={urls.external.githubIos}>aplikacja na iPhone'a</a>, <a href={urls.external.githubWeb}>strona internetowa</a>.</p>
-  },
-  {
-    question: "Chcę pomóc w projekcie",
-    answer: <p>Zapraszamy do włączenia się w pracę nad serwisem Pola. Dołącz do nas i pracuj nad <a href={urls.external.githubAndroid}>aplikacją na Androida</a>, <a href={urls.external.githubIos}>aplikacją na iPhone'a</a> lub <a href={urls.external.githubWeb}>stroną internetową</a>. Stale poszukujemy też wolontariuszy chcących pomóc nam rozwijać bazę danych o firmach - zachęcamy do kontaktu z Mateuszem Perowiczem (tel. 660 010 034, e-mail: mateusz.perowicz@klubjagiellonski.pl)</p>
-  },
-]
+const Wrapper = styled(Accordion)`
+  border-radius: 2px;
 
-const AccordionList = () => {
+  [hidden] {
+    display: none;
+  }
+`
+export interface ISingleAccordion {
+  question: string,
+  answer: JSX.Element,
+  key: number
+}
+
+export interface IAccordionList {
+  list: ISingleAccordion[]
+}
+
+const AccordionList: React.FC<IAccordionList> = ({ list }) => {
   return (
-    <Accordion allowMultipleExpanded allowZeroExpanded>
-      {faq.map((item, index) =>
-        <SingleAccordion question={item.question} answer={item.answer} key={index} />
+    <Wrapper allowMultipleExpanded allowZeroExpanded>
+      {list.map((item) =>
+        <SingleAccordion question={item.question} answer={item.answer} key={item.key} />
       )}
-    </Accordion>
+    </Wrapper>
   )
 }
 
--- src/components/accordion/SingleAccordion.tsx
+++ src/components/accordion/SingleAccordion.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components';
 
 import {
   AccordionItem,
@@ -6,26 +7,59 @@ import {
   AccordionItemButton,
   AccordionItemPanel,
 } from 'react-accessible-accordion';
+import { ISingleAccordion } from './AccordionList';
 
-import './accordion.css';
+const ItemButton = styled(AccordionItemButton)`
+  cursor: pointer;
+  padding: 10px;
+  width: 100%;
+  text-align: left;
+  border: none;
+  font-size: 14px;
+  font-weight: bolder;
 
-interface ISingleAccordion {
-  question: string,
-  answer: HTMLElement,
-  key: number
-}
+  ::before{
+    display: inline-block;
+    content: '';
+    height: 7px;
+    width: 7px;
+    margin-right: 12px;
+    border-bottom: 2px solid currentColor;
+    border-right: 2px solid currentColor;
+    transform: rotate(-135deg);
+  }
+
+  &[aria-expanded='true']::before, &[aria-selected='true']::before{
+    transform: rotate(45deg);
+  }
+`
+const fadein = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const ItemPanel = styled(AccordionItemPanel)`
+  padding: 10px;
+  font-size: 14px;
+  animation: ${fadein} 0.35s ease-in;
+  padding-left: 30px;
+`
 
 const SingleAccordion: React.FC<ISingleAccordion> = ({ question, answer, key }) => {
   return (
     <AccordionItem key={key}>
       <AccordionItemHeading>
-        <AccordionItemButton>
+        <ItemButton>
           {question}
-        </AccordionItemButton>
+        </ItemButton>
       </AccordionItemHeading>
-      <AccordionItemPanel>
+      <ItemPanel>
         {answer}
-      </AccordionItemPanel>
+      </ItemPanel>
     </AccordionItem>
   )
 }
