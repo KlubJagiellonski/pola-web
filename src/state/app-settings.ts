96d0cf6caa3d71c5ff9128211cd06c87e09747d1
Adam Walkowski <adam.walkowski@inperceptic.com>
2021-11-09 21:32:11 +0100

    PW-322: hide Huawei icon

--- src/state/app-settings.ts
+++ src/state/app-settings.ts
@@ -1,3 +1,8 @@
 export const AppSettings = {
+  mobile: {
+    SHOW_APPLE_STORE_LINK: true,
+    SHOW_GOOGLE_PLAY_LINK: true,
+    SHOW_HUAWEI_GALLERY_LINK: false,
+  },
   SHOW_POLISH_VALUE_NOTES: false,
 };
