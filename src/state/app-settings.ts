06a7d7cde62922ab70722efa095be116148a5aa8
Adam Walkowski <adam.walkowski@inperceptic.com>
2021-11-09 22:18:34 +0100

    PW-331: search on Enter keyboard key

--- src/state/app-settings.ts
+++ src/state/app-settings.ts
@@ -1,4 +1,10 @@
 export const AppSettings = {
+  search: {
+    SHOW_SUBMIT_BUTTON: true,
+    SHOW_BARCODE_ICON: false,
+    SHOW_VOICE_INPUT_ICON: false,
+    SEARCH_ON_INPUT_CHANGE: false,
+  },
   mobile: {
     SHOW_APPLE_STORE_LINK: true,
     SHOW_GOOGLE_PLAY_LINK: true,
