a8285abe0d9ec61f3449685713c6948f5ef89c8f
Adam Walkowski <adam.walkowski@inperceptic.com>
2022-06-19 13:24:32 +0200

    PW-532: update React plugins

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Loader from 'react-loader-spinner';
+import { Rings } from 'react-loader-spinner';
 import styled from 'styled-components';
-import { seconds } from '../domain/generic';
 import { color, pixels } from '../styles/theme';
 
 const Container = styled.div`
@@ -12,7 +11,6 @@ const Container = styled.div`
 
 interface ISpinner {
   text?: string;
-  timeout?: seconds;
   styles?: {
     size?: pixels;
     color?: string;
@@ -21,7 +19,7 @@ interface ISpinner {
 
 export const Spinner: React.FC<ISpinner> = ({ text, timeout, styles = { size: 80, color: color.button.red } }) => (
   <Container>
-    <Loader type="Rings" color={styles.color} height={styles.size} width={styles.size} timeout={timeout} />
+    <Rings color={styles.color} height={styles.size} width={styles.size} />
     {text && <label>{text}</label>}
   </Container>
 );
