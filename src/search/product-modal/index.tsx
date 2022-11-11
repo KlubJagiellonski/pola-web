28473030fe8db1791e0d5f76dcef94b07eca0aa1
Adam Walkowski <adam.walkowski@inperceptic.com>
2022-11-11 19:36:57 +0100

    Pw product modal webview fix (#651)

--- src/search/product-modal/index.tsx
+++ src/search/product-modal/index.tsx
@@ -3,14 +3,21 @@ import styled from 'styled-components';
 import { Product } from '../../domain/products';
 import { Modal } from '../../layout/modal/Modal';
 import { color, padding } from '../../styles/theme';
-import { ButtonFlavor, ButtonThemes } from '../../components/buttons/Button';
+import { Button, ButtonFlavor, ButtonThemes } from '../../components/buttons/Button';
 import { ClickOutside } from '../../utils/click-outside';
 import { ProductModalAction } from './ProductModalAction';
 import { navigateTo, openNewTab } from '../../utils/browser';
 import { ProductDetails } from './ProductDetails';
-import { urls } from '../../domain/website';
+import { MobilePagesPrefix, urls } from '../../domain/website';
 import { LinkButton } from '../../components/buttons/LinkButton';
 import { MobileApps } from '../../components/MobileApps';
+import { useLocation } from '@reach/router';
+
+const ButtonContainer = styled(Button)`
+  padding: ${padding.small};
+  min-width: 16rem;
+  border-radius: 0;
+`;
 
 const Actions = styled.div`
   display: flex;
@@ -45,6 +52,8 @@ interface IProductModal {
 }
 
 export const ProductModal: React.FC<IProductModal> = ({ product, onClose }) => {
+  const location = useLocation();
+  const isMobile = location.href.indexOf(`/${MobilePagesPrefix}/`) > -1;
   const handleReport = () => {
     onClose();
     navigateTo(urls.pola.home('contact'));
@@ -62,22 +71,50 @@ export const ProductModal: React.FC<IProductModal> = ({ product, onClose }) => {
     <ClickOutside clickOutsideHandler={onClose}>
       <Modal title={`EAN: ${product.code}`} onClose={onClose}>
         <ProductDetails product={product} />
-        <Actions>
-          <LinkButton label="Zobacz przyjaciół Poli" styles={ButtonThemes.WhiteRed} onClick={redirectToFriends} />
-          {product.donate.show_button && (
-            <LinkButton label="Potrzebujemy 1 ZŁ" styles={ButtonThemes.Red} onClick={handleDonate} />
-          )}
-        </Actions>
-        <ProductModalAction action={{ label: product.report.button_text, callback: handleReport }}>
-          <span>Posiadasz aktualne dane na temat tego produktu?</span>
-        </ProductModalAction>
-        <ProductModalAction
-          theme={{ backgroundColor: color.background.white, buttonTheme: ButtonThemes[ButtonFlavor.RED] }}>
-          <AppDownload>
-            <p>Zabierz Polę na zakupy! Skanuj kody w aplikacji:</p>
-            <MobileApps size={64} />
-          </AppDownload>
-        </ProductModalAction>
+        {isMobile ? (
+          <Actions>
+            <a href={`https://pola-app.pl/${urls.pola.friends()}`} target="_blank">
+              <ButtonContainer theme={ButtonThemes.WhiteRed}>
+                <span>Zobacz przyjaciół Poli</span>
+              </ButtonContainer>
+            </a>
+            <a href={product.donate.url} target="_blank">
+              <ButtonContainer theme={ButtonThemes.Red}>
+                <span>Potrzebujemy 1 ZŁ1</span>
+              </ButtonContainer>
+            </a>
+          </Actions>
+        ) : (
+          <Actions>
+            <LinkButton label="Zobacz przyjaciół Poli" styles={ButtonThemes.WhiteRed} onClick={redirectToFriends} />
+            {product.donate.show_button && (
+              <LinkButton label="Potrzebujemy 1 ZŁ1" styles={ButtonThemes.Red} onClick={handleDonate} />
+            )}
+          </Actions>
+        )}
+        {isMobile ? (
+          <Actions>
+            <span>Posiadasz aktualne dane na temat tego produktu?</span>
+            <a href={`https://pola-app.pl/${urls.pola.home('contact')}`} target="_blank">
+              <ButtonContainer theme={ButtonThemes.Red}>
+                <span>{product.report.button_text}</span>
+              </ButtonContainer>
+            </a>
+          </Actions>
+        ) : (
+          <ProductModalAction action={{ label: product.report.button_text, callback: handleReport }}>
+            <span>Posiadasz aktualne dane na temat tego produktu?</span>
+          </ProductModalAction>
+        )}
+        {!isMobile && (
+          <ProductModalAction
+            theme={{ backgroundColor: color.background.white, buttonTheme: ButtonThemes[ButtonFlavor.RED] }}>
+            <AppDownload>
+              <p>Zabierz Polę na zakupy! Skanuj kody w aplikacji:</p>
+              <MobileApps size={64} />
+            </AppDownload>
+          </ProductModalAction>
+        )}
       </Modal>
     </ClickOutside>
   );
