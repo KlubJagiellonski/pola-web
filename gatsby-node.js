9fceca4b641f12873f22ea579b0e0b02c20ad141
zyzik1248 <62909958+zyzik1248@users.noreply.github.com>
2023-06-29 23:13:51 +0200

    contentful friends (#777)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,24 @@
 const path = require('path');
-const { createFilePath } = require(`gatsby-source-filesystem`);
+// const { createFilePath } = require(`gatsby-source-filesystem`);
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` });
-    const separtorIndex = ~slug.indexOf('--') ? slug.indexOf('--') : 0;
-    const shortSlugStart = separtorIndex ? separtorIndex + 2 : 0;
-    createNodeField({
-      node,
-      name: `slug`,
-      value: `${separtorIndex ? '/' : ''}${slug.substring(shortSlugStart)}`,
-    });
-    createNodeField({
-      node,
-      name: `prefix`,
-      value: separtorIndex ? slug.substring(1, separtorIndex) : '',
-    });
-  }
+  // if (node.internal.type === `MarkdownRemark`) {
+  //   const slug = createFilePath({ node, getNode, basePath: `pages` });
+  //   const separtorIndex = ~slug.indexOf('--') ? slug.indexOf('--') : 0;
+  //   const shortSlugStart = separtorIndex ? separtorIndex + 2 : 0;
+  //   createNodeField({
+  //     node,
+  //     name: `slug`,
+  //     value: `${separtorIndex ? '/' : ''}${slug.substring(shortSlugStart)}`,
+  //   });
+  //   createNodeField({
+  //     node,
+  //     name: `prefix`,
+  //     value: separtorIndex ? slug.substring(1, separtorIndex) : '',
+  //   });
+  // }
 };
 
 exports.createPages = async function ({ graphql, actions }) {
