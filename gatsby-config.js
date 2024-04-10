c43e68d05478ec9dd4be8bbd3562d3a67f54c30d
zyzik1248 <zuzannadudzinska96@gmail.com>
2024-04-10 23:42:07 +0200

    add doenload local

--- gatsby-config.js
+++ gatsby-config.js
@@ -217,6 +217,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        downloadLocal: true,
       },
     },
   ],
