3bea632255a1b39462abff8a62825703acca2c12
Zuzanna Dudzińska <zuzanna.dudzinska@sarigato.com>
2025-06-20 18:16:01 +0200

    fix show product

--- src/layout/WebViewLayout.tsx
+++ src/layout/WebViewLayout.tsx
@@ -10,24 +10,28 @@ import { appDispatcher } from '@App/state/app-dispatcher';
 
 import { SearchInfoModal } from 'search/components/form/SearchInfoModal';
 import { ProductModal } from 'search/components/product-modal';
-import { searchDispatcher } from 'search/state/search-dispatcher';
-import { SearchStateName } from 'search/state/search-reducer';
 
 import { CustomScrollbarDiv } from './CustomScrollbar';
 import { StateLoader } from './StateLoader';
+import { selectedProductDispatcher } from 'search/state/selected-product-dispatcher';
+
 
 const connector = connect(
   (state: IPolaState) => {
-    const { app, search } = state;
+    const { app, selectedProduct } = state;
     return {
       isSearchInfoVisible: app.isSearchInfoVisible,
       activePage: app.activePage,
-      selectedProduct: search.stateName === SearchStateName.SELECTED ? search.selectedProduct : undefined,
+      selectedProduct: selectedProduct.product,
     };
   },
   {
     toggleSearchInfo: appDispatcher.toggleSearchInfo,
-    unselectProduct: searchDispatcher.unselectProduct,
+
+    selectProduct: selectedProductDispatcher.selectProduct,
+
+    // TODO: move modals' actions somewhere else
+    unselectProduct: selectedProductDispatcher.unselectProduct,
   }
 );
 
@@ -64,15 +68,17 @@ const Layout: React.FC<IPageLayout> = ({
   toggleSearchInfo,
   unselectProduct,
   styles,
-}) => (
-  <ErrorBoundary scope="webview-layout">
-    <StateLoader />
-    <LayoutContainer id="layout-container">
-      {selectedProduct && <ProductModal product={selectedProduct} onClose={unselectProduct} />}
-      {isSearchInfoVisible && <SearchInfoModal onClose={toggleSearchInfo} />}
-      <PageContent {...styles}>{children}</PageContent>
-    </LayoutContainer>
-  </ErrorBoundary>
-);
+}) => {
+  return(
+    <ErrorBoundary scope="webview-layout">
+      <StateLoader />
+      <LayoutContainer id="layout-container">
+        {selectedProduct && <ProductModal product={selectedProduct} onClose={unselectProduct} />}
+        {isSearchInfoVisible && <SearchInfoModal onClose={toggleSearchInfo} />}
+        <PageContent {...styles}>{children}</PageContent>
+      </LayoutContainer>
+    </ErrorBoundary>
+  )
+};
 
 export const WebViewLayout = connector(Layout);
